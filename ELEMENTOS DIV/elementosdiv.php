626c9c662e4eae867c7e9138e546986448535e6f
Valeriaalfaro05 <paulavaleria.alfarocoronado@unidemat.edu.mx>
2025-06-23 06:48:09 -0600

    Add files via upload

---
+++ ELEMENTOS DIV/elementosdiv.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Modelo de cajas de CSS</title>
+    <link rel="stylesheet" href="css/modelo-de-cajas2.css">
+</head>
+<body>
+    <h1>Ejemplo de Modelo de Cajas con DIV</h1>
+
+    <!-- Primer DIV decorativo -->
+    <div class="caja-roja">
+        <p>Esta es una caja roja con márgenes, bordes y relleno.</p>
+    </div>
+
+    <br>
+
+    <!-- Segundo DIV con un ID específico -->
+    <div id="modeloCaja">
+        <p>Este es otro DIV con estilo personalizado usando ID.</p>
+    </div>
+</body>
+</html>
